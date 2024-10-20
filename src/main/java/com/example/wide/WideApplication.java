99f492c35b5ad9886fa2ba827ab4d1757e2cdab4
devEunSeon <eslee0328@gmail.com>
2024-10-20 21:05:44 +0900

    init commit

---
+++ src/main/java/com/example/wide/WideApplication.java
@@ -0,0 +1,13 @@
+package com.example.wide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WideApplication.class, args);
+	}
+
+}
