dd9074f6e3a708ff879b9f02100a9af1ad1b83d3
devEunSeon <eslee0328@gmail.com>
2024-10-21 22:06:13 +0900

    login form 수정 및 index form 추가

--- src/main/java/com/example/wide/controller/HomeController.java
+++ src/main/java/com/example/wide/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.example.wide.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -9,7 +12,16 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         System.out.println("HOME Start");
-        return "index";
+        return "login";
     }
 
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(HttpServletRequest request) {
+        String id = request.getParameter("userId");
+        if(id.equals("admin") || id.equals("admin123")) {
+            return "login";
+        }else {
+           return "index";
+        }
+    }
 }
